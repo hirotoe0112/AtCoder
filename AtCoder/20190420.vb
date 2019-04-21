ea7b39fddea98b41404ce74e53d62ee7867eb4ec
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2019-04-21 23:43:33 +0900

    CreateNewRepository

---
+++ AtCoder/20190420.vb
@@ -0,0 +1,154 @@
+﻿Module _20190420
+
+
+End Module
+
+Module Past_20190420
+
+    Sub B_未提出者は誰だ()
+        'https://atcoder.jp/contests/joi2007yo/tasks/joi2007yo_b
+
+        Dim arrStudentCount(28) As Integer
+
+        For i As Integer = 0 To 27
+
+            arrStudentCount(i) = CInt(Console.ReadLine())
+
+        Next
+
+        Dim arrNotSubmit(2) As Integer
+        Dim iDiscover As Integer = 0
+        For j As Integer = 1 To 30
+
+            If Array.IndexOf(arrStudentCount.ToArray, j) < 0 Then
+
+                arrNotSubmit(iDiscover) = j
+                iDiscover = iDiscover + 1
+
+            End If
+
+        Next
+
+        If arrNotSubmit(0) > arrNotSubmit(1) Then
+
+            Console.WriteLine(arrNotSubmit(1))
+            Console.WriteLine(arrNotSubmit(0))
+
+        Else
+
+            Console.WriteLine(arrNotSubmit(0))
+            Console.WriteLine(arrNotSubmit(1))
+
+        End If
+
+    End Sub
+
+    Sub B_Great_Ocean_View()
+        'https://atcoder.jp/contests/abc124/tasks/abc124_b
+
+        '山の数
+        Dim intMountainCount As Integer = Console.ReadLine()
+
+        '高さ
+        Dim sHeight As String() = Console.ReadLine().Split(" ")
+
+        Dim iMaxHeight As Integer = CInt(sHeight(0))
+        Dim iWatchCount As Integer = 0
+        'For文でAs Integerをつけないとコンパイルエラーになる！！
+        For i As Integer = 0 To intMountainCount - 1
+
+            If CInt(sHeight(i)) >= iMaxHeight Then
+
+                iWatchCount = iWatchCount + 1
+
+                iMaxHeight = CInt(sHeight(i))
+
+            End If
+
+        Next
+
+        Console.WriteLine(iWatchCount)
+
+    End Sub
+
+    Sub A_Buttons()
+        'https://atcoder.jp/contests/abc124/tasks/abc124_a
+
+        '入力を受け取る
+        Dim a As String = Console.ReadLine()
+
+        '数値に分解
+        Dim Int1 As Integer = CInt(a.Split(" ")(0))
+        Dim Int2 As Integer = CInt(a.Split(" ")(1))
+
+        If Int1 = Int2 Then
+
+            '出力
+            Console.WriteLine(Int1 + Int2)
+
+        End If
+
+        If Int1 > Int2 Then
+
+            '出力
+            Console.WriteLine(Int1 + Int1 - 1)
+
+        End If
+
+        If Int2 > Int1 Then
+
+            '出力
+            Console.WriteLine(Int2 + Int2 - 1)
+
+        End If
+
+    End Sub
+
+    Sub ABC081A_Product()
+        'https://atcoder.jp/contests/abs/tasks/abc086_a
+
+        '入力を受け取る
+        Dim a As String = Console.ReadLine()
+
+        '数値に分解
+        Dim Int1 As Integer = CInt(a.Split(" ")(0))
+        Dim Int2 As Integer = CInt(a.Split(" ")(1))
+
+        'どちらかが偶数なら偶数
+        If Int1 Mod 2 = 0 Then
+
+            '出力
+            Console.WriteLine("Even")
+
+        ElseIf Int2 Mod 2 = 0 Then
+
+            '出力
+            Console.WriteLine("Even")
+
+        Else
+
+            '出力
+            Console.WriteLine("Odd")
+
+        End If
+
+    End Sub
+
+    Sub practice_contest_A()
+        'https://atcoder.jp/contests/practice/tasks/practice_1
+
+        '1行目
+        Dim a As Integer = Console.ReadLine()
+
+        '2行目
+        Dim b As String() = Console.ReadLine().Split(" ")
+
+        '3行目
+        Dim c As String = Console.ReadLine()
+
+        '出力
+        Console.WriteLine(CInt(a) + CInt(b(0)) + CInt(b(1)) & " " + c)
+
+    End Sub
+
+End Module
---
+++ AtCoder/20190421.vb
@@ -0,0 +1,153 @@
+﻿Module _20190421
+
+    Sub Main()
+
+
+
+    End Sub
+
+End Module
+
+Module Past_20190421
+
+    Sub A_Double_Helix()
+        'https://atcoder.jp/contests/abc122/tasks/abc122_a
+
+        Dim sRead As String = Console.ReadLine()
+
+        Select Case sRead
+            Case "A"
+                Console.WriteLine("T")
+            Case "T"
+                Console.WriteLine("A")
+            Case "C"
+                Console.WriteLine("G")
+            Case "G"
+                Console.WriteLine("C")
+        End Select
+
+    End Sub
+
+    Sub A_Five_Antennas()
+        'https://atcoder.jp/contests/abc123/tasks/abc123_a
+
+        Dim iAntena(5) As Integer
+        iAntena(0) = Console.ReadLine()
+        Dim iMax As Integer = iAntena(0)
+        Dim iMin As Integer = iAntena(0)
+
+        For i As Integer = 1 To 4
+
+            iAntena(i) = Console.ReadLine()
+
+            If iAntena(i) > iMax Then
+
+                iMax = iAntena(i)
+
+            ElseIf iAntena(i) < iMin Then
+
+                iMin = iAntena(i)
+
+            End If
+
+        Next
+
+        Dim iDistance As Integer = Console.ReadLine()
+
+        If iMax - iMin > iDistance Then
+
+            Console.WriteLine(":(")
+
+        Else
+
+            Console.WriteLine("Yay!")
+
+        End If
+
+    End Sub
+
+    Sub abc016_1()
+        'https://atcoder.jp/contests/abc016/tasks/abc016_1
+
+        Dim sRead As String = Console.ReadLine()
+
+        Dim iMonth As Integer = sRead.Split(" ")(0)
+        Dim iDay As Integer = sRead.Split(" ")(1)
+
+        If iMonth Mod iDay = 0 Then
+
+            Console.WriteLine("YES")
+
+        Else
+
+            Console.WriteLine("NO")
+
+        End If
+
+    End Sub
+
+    Sub tenka1_2019_b()
+        'https://atcoder.jp/contests/tenka1-2019-beginner/tasks/tenka1_2019_b
+
+        '文字列の長さ
+        Dim iStrLength As Integer = CInt(Console.ReadLine())
+
+        '文字列
+        Dim sStr As String = Console.ReadLine()
+
+        '整数
+        Dim iNumber As Integer = CInt(Console.ReadLine())
+
+        '文字列を分解
+        Dim arrStr() As Char = sStr.ToCharArray
+
+        '比較する文字を取得
+        Dim sCompareStr As String = arrStr(iNumber - 1)
+
+        '出力文字列
+        Dim sOutput As String = ""
+
+        For i As Integer = 0 To iStrLength - 1
+
+            If arrStr(i) = sCompareStr Then
+
+                sOutput = sOutput & arrStr(i)
+
+            Else
+
+                sOutput = sOutput & "*"
+
+            End If
+
+        Next
+
+        Console.WriteLine(sOutput)
+
+    End Sub
+
+    Sub A_On_the_Way()
+        'https://atcoder.jp/contests/tenka1-2019-beginner/tasks/tenka1_2019_a
+
+        Dim sRead As String = Console.ReadLine()
+
+        Dim iHouse1 As Integer = sRead.Split(" ")(0)
+        Dim iHouse2 As Integer = sRead.Split(" ")(1)
+        Dim iHouse3 As Integer = sRead.Split(" ")(2)
+
+        If iHouse1 < iHouse3 AndAlso iHouse3 < iHouse2 Then
+
+            Console.WriteLine("Yes")
+
+        ElseIf iHouse1 > iHouse3 AndAlso iHouse3 > iHouse2 Then
+
+            Console.WriteLine("Yes")
+
+        Else
+
+            Console.WriteLine("No")
+
+        End If
+
+    End Sub
+
+End Module
