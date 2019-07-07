d6ecaf37b65326c2c3deef8bc71690b9b84bc578
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2019-07-07 18:30:09 +0900

    20190707

---
+++ AtCoder/20190525.vb
@@ -0,0 +1,96 @@
+﻿Module _20190525
+
+End Module
+
+Module Past_20190525
+
+    Sub Prison()
+        'https://atcoder.jp/contests/abc127/tasks/abc127_c
+
+        Dim sRead As String = Console.ReadLine()
+        Dim N As Integer = sRead.Split(" ")(0)
+        Dim M As Integer = sRead.Split(" ")(1)
+
+        Dim sFirst As String = Console.ReadLine()
+
+        Dim iMin As Integer = sFirst.Split(" ")(0)
+        Dim iMax As Integer = sFirst.Split(" ")(1)
+
+        For i As Integer = 2 To M
+
+            Dim sTemp As String = Console.ReadLine()
+
+            If CInt(sTemp.Split(" ")(0)) > iMin Then
+
+                iMin = CInt(sTemp.Split(" ")(0))
+
+            End If
+
+            If CInt(sTemp.Split(" ")(1)) < iMax Then
+
+                iMax = CInt(sTemp.Split(" ")(1))
+
+            End If
+
+        Next
+
+        If iMax - iMin + 1 < 0 Then
+
+            Console.WriteLine(0)
+
+        Else
+
+            Console.WriteLine(iMax - iMin + 1)
+
+        End If
+
+    End Sub
+
+    Sub Algae()
+        'https://atcoder.jp/contests/abc127/tasks/abc127_b
+
+        Dim sRead As String = Console.ReadLine()
+
+        Dim r As Integer = sRead.Split(" ")(0)
+        Dim D As Integer = sRead.Split(" ")(1)
+        Dim X2000 As Integer = sRead.Split(" ")(2)
+
+        Dim XNext As Integer = r * X2000 - D
+
+        Console.WriteLine(XNext)
+
+        For i As Integer = 2 To 10
+
+            XNext = r * XNext - D
+
+            Console.WriteLine(XNext)
+
+        Next
+
+    End Sub
+
+    Sub Ferris_Wheel()
+        'https://atcoder.jp/contests/abc127/tasks/abc127_a
+
+        Dim sRead As String = Console.ReadLine()
+
+        Dim iAge As Integer = sRead.Split(" ")(0)
+        Dim iFee As Integer = sRead.Split(" ")(1)
+
+        If iAge >= 13 Then
+
+            Console.WriteLine(iFee)
+
+        ElseIf iAge >= 6 Then
+
+            Console.WriteLine(iFee / 2)
+
+        Else
+
+            Console.WriteLine(0)
+
+        End If
+
+    End Sub
+
+End Module
---
+++ AtCoder/20190526.vb
@@ -0,0 +1,28 @@
+﻿Module _20190526
+
+    Sub Main()
+        'https://atcoder.jp/contests/abc128/tasks/abc128_b
+
+
+
+    End Sub
+
+End Module
+
+Module Past_20190526
+
+    Sub Apple_Pie()
+        'https://atcoder.jp/contests/abc128/tasks/abc128_a
+
+        Dim sRead As String = Console.ReadLine()
+
+        Dim iApple As Integer = sRead.Split(" ")(0)
+        Dim iPart As Integer = sRead.Split(" ")(1)
+
+        Dim iAll As Integer = iApple * 3 + iPart
+
+        Console.WriteLine(iAll \ 2)
+
+    End Sub
+
+End Module
